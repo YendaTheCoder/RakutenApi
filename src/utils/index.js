96aba32eba398821ced2e19d49311ed2013890b0
Dylan Cooper <dylanc55@Dylans-MacBook-Air.local>
2020-08-19 14:16:56 +0900

    remove two helper funcs

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,33 +6,3 @@ export async function getDbJokes() {
   console.log("data", data);
   return data;
 }
-
-// // store new joke in database
-// export function storeJoke () {
-//     const joke = {
-//       joke_ID: this.joke.key,
-//       joke: this.joke,
-//       like: "",
-//       dislike: "",
-//     }
-//     fetch(/*"/api/endpoint"*/, {
-//       method: "post",
-//       body: JSON.stringify(joke),
-//       headers: { 'Content-type': 'application/json' }
-//     });
-// }
-
-// // update existing joke in database
-// export function updateJoke() {
-//     const joke = {
-//       joke_ID: this.joke.key,
-//       joke: this.joke,
-//       like: "",
-//       dislike: "",
-//     }
-//     fetch(/*"/api/endpoint"*/, {
-//       method: "patch",
-//       body: JSON.stringify(joke),
-//       headers: { 'Content-type': 'application/json' }
-//     });
-// }
