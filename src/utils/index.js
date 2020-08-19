0641cf84a5184043d7a559d3ec6e57e554db7f2b
Dylan Cooper <dylanc55@Dylans-MacBook-Air.local>
2020-08-19 11:48:29 +0900

    helper function store new joke, update existing joke

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,38 @@
 import axios from "axios";
 
 // retrieve jokes from database
-export async function getJokes() {
+export async function getDbJokes() {
     const { data } = await axios.get(/*"endpoint"*/);
     console.log("data", data);
     return data;
+}
+
+// store new joke in database
+export function storeJoke () {
+    const joke = {
+      joke_ID: this.joke.key,
+      joke: this.joke,
+      like: "",
+      dislike: "",
+    }
+    fetch(/*"/api/endpoint"*/, {
+      method: "post",
+      body: JSON.stringify(joke),
+      headers: { 'Content-type': 'application/json' }
+    });
+}
+
+// update existing joke in database
+export function updateJoke() {
+    const joke = {
+      joke_ID: this.joke.key,
+      joke: this.joke,
+      like: "",
+      dislike: "",
+    }
+    fetch(/*"/api/endpoint"*/, {
+      method: "patch",
+      body: JSON.stringify(joke),
+      headers: { 'Content-type': 'application/json' }
+    });
 }
